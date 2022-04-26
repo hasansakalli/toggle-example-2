f5ca55c0d0b8879b00ebf547686ca571197f6ff7
HASAN SAKALLI <89208116+hasansakalli@users.noreply.github.com>
2022-04-26 08:52:57 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+const pTag= document.querySelector("p");
+const h1box= document.querySelector("h1");
+const h2box= document.querySelector("h2");
+const h3box= document.querySelector("h3");
+const h4box= document.querySelector("h4");
+const h5box= document.querySelector("h5");
+const spanId= document.querySelectorAll("span");
+
+
+function openP(){
+  pTag.classList.toggle("p2");
+     
+}
+function openH1(){
+    h1box.classList.toggle("h1box");
+        
+  }
+  function openH2(){
+    h2box.classList.toggle("h2box");
+       
+  
+  }
+  function openH3(){
+    h3box.classList.toggle("h3box");
+   
+   
+  
+  }
+  function openH4(){
+    h4box.classList.toggle("h4box");
+    
+   
+  
+  }
+  function openH5(){
+    h5box.classList.toggle("h5box");
+  
+  
+  
+  }
